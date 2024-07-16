a51bf6a5cd1776883d183bdb5e56cb917bdccc98
Bagishan <Bagishan01@gmail.com>
2024-07-16 11:18:22 +0600

    hot fix 10

--- backend/achievements/models.py
+++ backend/achievements/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Achievements(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Название')
+    title = models.CharField(max_length=200, verbose_name='Название')
     body = models.TextField(verbose_name='Описание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
