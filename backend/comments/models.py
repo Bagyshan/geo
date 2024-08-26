9a51127bf3bbcf17bfdcfebbb808598c56252340
Bagyshan <Bagishan01@gamil.com>
2024-08-26 20:26:12 +0600

    pre fix commit

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -22,9 +22,28 @@ class Comment(models.Model):
         verbose_name_plural = 'Комментарии'
 
 
+# class ApplicationType(models.Model):
+#     name = models.CharField(max_length=50, verbose_name='Название')
+#     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+#     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+#     def __str__(self) -> str:
+#         return self.name
+    
+#     class Meta:
+#         verbose_name = 'Тип заявки'
+#         verbose_name_plural = 'Тип заявок'
+
+
 class KyrgyzGeologyApplication(models.Model):
+    APPLICATION_CHOICES = [
+        ('kyrgyzgeology_applicaiton', 'Заявка Кыргызгеологии'),
+        ('boezgrt_application', 'Заявка БОЭЗГРТ')
+    ]
+
     username = models.CharField(max_length=50, verbose_name='Имя')
     email = models.EmailField(verbose_name='Email')
+    application_type = models.CharField(max_length=100, choices=APPLICATION_CHOICES, verbose_name='Тип заявки')
     content = models.TextField(verbose_name='Сообщение')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата отправки')
     file = models.FileField(upload_to='application_files', null=True, blank=True, verbose_name='Файл')
@@ -37,16 +56,16 @@ class KyrgyzGeologyApplication(models.Model):
         verbose_name_plural = 'Заявки Кыргызгеология'
 
 
-class BoezgrtApplication(models.Model):
-    username = models.CharField(max_length=50, verbose_name='Имя')
-    email = models.EmailField(verbose_name='Email')
-    content = models.TextField(verbose_name='Сообщение')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата отправки')
-    file = models.FileField(upload_to='application_files', null=True, blank=True, verbose_name='Файл')
+# class BoezgrtApplication(models.Model):
+#     username = models.CharField(max_length=50, verbose_name='Имя')
+#     email = models.EmailField(verbose_name='Email')
+#     content = models.TextField(verbose_name='Сообщение')
+#     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата отправки')
+#     file = models.FileField(upload_to='application_files', null=True, blank=True, verbose_name='Файл')
 
-    def __str__(self) -> str:
-        return self.content
+#     def __str__(self) -> str:
+#         return self.content
     
-    class Meta:
-        verbose_name = 'Заявка Боэзгрт'
-        verbose_name_plural = 'Заявки Боэзгрт'
+#     class Meta:
+#         verbose_name = 'Заявка Боэзгрт'
+#         verbose_name_plural = 'Заявки Боэзгрт'
--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from .models import Comment, KyrgyzGeologyApplication, BoezgrtApplication
+from .models import Comment, KyrgyzGeologyApplication
 
+
+# class ApplicationTypeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ApplicationType
+#         fields = '__all__'        
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,14 +13,12 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         read_only_fields = ['news']
 
-
 class KyrgyzGeologyApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = KyrgyzGeologyApplication
         fields = '__all__'
 
-
-class BoezgrtApplicationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BoezgrtApplication
-        fields = '__all__'
+# class BoezgrtApplicationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BoezgrtApplication
+#         fields = '__all__'
